c81708cf0846a889869d02dec915cdd5e0a66b37
chenxing <1057791979@qq.com>
2023-07-14 11:07:35 +0800

    network

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,20 +9,23 @@ module.exports = {
         // 配置页面左上角的logo，/为.vuepress下的public文件夹
         logo: '/assets/img/logo.png',
         nav: [
-            { 
+            {
                 text: 'Java',
                 ariaLabel: 'Java Menu',
                 // link: "/java/javase/"
                 // 使用items而不是link可以变成一个下拉框
                 // 以 / 结尾的路径将会被视为 */README.md
                 items: [
-                    {text: "基础", link: "/java/"},
-                    {text: "JVM", link: "/jvm/"},
+                    { text: "基础", link: "/java/" },
+                    { text: "JVM", link: "/jvm/" },
                 ]
             },
             { text: '框架', link: '/frame/' },
             { text: '算法', link: 'https://baidu.com', target: "_self" },
-            { text: '网络', link: 'https://baidu.com' },
+            {
+                text: '网络',
+                link: '/network/Network-Protocol.html'
+            },
         ],
         sidebar: {
             '/java/': [
@@ -45,7 +48,7 @@ module.exports = {
                 'StackFrame',
                 'About-Reference',
                 'Handle-Promotion'
-            ]     
+            ]
         },
         sidebarDepth: 2,
         lastUpdated: 'Last Updated'
